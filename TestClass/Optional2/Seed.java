ffd880b2261e49c5883963263207beda5d806b56
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-10 15:05:57 -0800

    Create Seed.java

---
+++ TestClass/Optional2/Seed.java
@@ -0,0 +1,21 @@
+import java.util.Optional;
+ 
+public class Seed {
+    private String name;
+ 
+    public Seed(String name) {
+        this.name = name;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+     
+    public Optional<Fruit> growIntoFruit() {
+        return Optional.of(new Fruit(this));
+    }
+     
+    public Optional<Fruit> growIntoFruit(Integer price) {
+        return Optional.of(new Fruit(this, price));
+    }
+}
