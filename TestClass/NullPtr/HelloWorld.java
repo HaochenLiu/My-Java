4b5846f46b48f95e082cae730adffd62bea7bb4c
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-10 13:18:42 -0800

    Create HelloWorld.java

---
+++ TestClass/NullPtr/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main(String args[]){
+        AutoBicycle a = new AutoBicycle(1, 2, 3, 4);
+        a.showStatus();
+        a.clear();
+        a.showStatus();
+    }
+}
