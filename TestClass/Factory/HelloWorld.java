09069680919035aed9ec5610f93ec5dc8e8fb78e
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-19 15:54:23 -0800

    Update HelloWorld.java

--- TestClass/Factory/HelloWorld.java
+++ TestClass/Factory/HelloWorld.java
@@ -1,9 +1,10 @@
-public class HelloWorld
-{
-    public static void main(String args[]){
-        Car benz = new Benz();
-        Car audi = new Audi();
-        benz.drive();
-        audi.drive();
-    }
+public class HelloWorld {
+  
+  public static void main(String args[]){
+    Car benz = new Benz();
+    Car audi = new Audi();
+    benz.drive();
+    audi.drive();
+  }
+  
 }
