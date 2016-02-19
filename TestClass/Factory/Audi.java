cf106c2ce982d90ceaf29f8fbe70f79f420b0cea
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-19 15:53:27 -0800

    Create Audi.java

---
+++ TestClass/Factory/Audi.java
@@ -0,0 +1,10 @@
+public class Audi implements Car {
+
+  @Override
+  public int drive() {
+    // TODO(haochenl): Auto-generated method stub
+    System.out.println("drive Audi");
+    return 0;
+  }
+  
+}
