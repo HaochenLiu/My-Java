c04e573f733da384d3991916e64abffb22281bbc
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-19 15:52:52 -0800

    Create HelloWorld.java

---
+++ TestClass/Factory/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main(String args[]){
+        Car benz = new Benz();
+        Car audi = new Audi();
+        benz.drive();
+        audi.drive();
+    }
+}
