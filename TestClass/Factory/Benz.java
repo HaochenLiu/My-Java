4b5e218c0a13683080c362d98be80cbd3528e01e
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-19 15:53:14 -0800

    Create Benz.java

---
+++ TestClass/Factory/Benz.java
@@ -0,0 +1,10 @@
+public class Benz implements Car {
+
+  @Override
+  public int drive() {
+    // TODO(haochenl): Auto-generated method stub
+    System.out.println("drive Benz");
+    return 0;
+  }
+  
+}
